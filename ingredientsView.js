0b4407376577d6498707d1d483472181c9d867d3
anis <medanis.lakhdar@gmail.com>
2023-02-18 15:54:06 +0100

    refactor architecture

--- ingredientsView.js
+++ ingredientsView.js
@@ -1,23 +1,37 @@
 
+/**
+ * Représente la vue pour la sélection des ingrédients.
+ */
 class IngredientsView {
     constructor() {
         this.dropdownContainer = document.getElementById("ingredient-dropdown");
+        this.ingredientList = document.getElementById("ingredient-list");
         this.tagContainer = document.getElementById("ingredient-tags");
+        this.searchInput = document.getElementById("ingredient-search");
     }
     
+   /**
+   * Génère un dropdown avec les ingrédients et une barre de recherche pour filtrer les ingredients.
+   * @param {Array<Object>} ingredients - La liste des ingrédients.
+   */
     renderIngredientDropdown(ingredients) {
-        this.dropdownContainer.innerHTML = "";
-        ingredients.forEach((ingredient) => {
+        this.ingredientList.innerHTML = "";
+        const filteredIngredients = this.filterIngredients(ingredients);
+        filteredIngredients.forEach((ingredient) => {
             const button = document.createElement("button");
             button.classList.add("ingredient-button");
             button.innerHTML = ingredient.ingredient;
             button.addEventListener("click", () => {
                 this.addIngredient(ingredient);
             });
-            this.dropdownContainer.appendChild(button);
+            this.ingredientList.appendChild(button);
         });
     }
     
+    /**
+    * Ajoute un ingrédient à la liste des ingrédients (tags) sélectionnés.
+    * @param {Object} ingredient - L'ingrédient à ajouter.
+    */
     addIngredient(ingredient) {
         const tag = document.createElement("span");
         tag.classList.add("ingredient-tag");
@@ -31,6 +45,10 @@ class IngredientsView {
         this.tagContainer.dispatchEvent(ingredientAddedEvent);
     }
     
+    /**
+    * Retire un ingrédient de la liste des ingrédients sélectionnés.
+    * @param {Object} ingredient - L'ingrédient à retirer.
+    */
     removeIngredient(ingredient) {
         const ingredientTags = Array.from(this.tagContainer.children);
         const tagToRemove = ingredientTags.find((tag) => tag.innerHTML === ingredient.ingredient);
@@ -39,9 +57,34 @@ class IngredientsView {
         const ingredientRemovedEvent = new CustomEvent("ingredient-removed", { detail: ingredient });
         this.tagContainer.dispatchEvent(ingredientRemovedEvent);
     }
+
+    /**
+    * Filtre les ingrédients en fonction de la recherche de l'utilisateur.
+    * @param {Array<Object>} ingredients - La liste des ingrédients.
+    * @returns {Array<Object>} La liste des ingrédients filtrée.
+    */
+    filterIngredients(ingredients) {
+        const searchTerm = this.searchInput.value.toLowerCase();
+        return ingredients.filter((ingredient) => ingredient.ingredient.toLowerCase().includes(searchTerm));
+    }
     
+    /**
+    * Met à jour le dropdown des ingrédients 
+    * @param {Array<Object>} ingredients - La liste des ingrédients.
+    */
     updateIngredientDropdown(ingredients) {
         this.renderIngredientDropdown(ingredients);
+    }
+
+    /**
+    * Associe l'événement de filtre à la recherche d'ingrédients.
+    * @param {Function} searchHandler - La fonction à exécuter lors de la recherche d'ingrédients.
+    */
+    bindFilterEvent(searchHandler) {
+        this.searchInput.addEventListener("input", () => {
+            const searchTerm = this.searchInput.value;
+            searchHandler(searchTerm);
+        });
     }
     
 }
--- recipesView.js
+++ recipesView.js
@@ -1,7 +1,15 @@
+/**
+ * Représente une vue pour afficher une liste de recettes.
+ */
 class RecipesView {
     constructor() {
         this.recipesList = document.getElementById("recipes-list");
     }
+
+     /**
+     * Met à jour la vue avec une liste de recettes.
+     * @param {Array} recipes - La liste des recettes à afficher.
+     */
     updateView(recipes) {
         // Clear the existing list
         this.recipesList.innerHTML = ""; 
@@ -19,5 +27,5 @@ class RecipesView {
     
 }
 
-// pour avoir une seule instance 
+// Exporte une instance unique de la classe RecipesView
 export default new RecipesView();
