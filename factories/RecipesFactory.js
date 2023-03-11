2b7dce07ed97989ab6bf27e97f5703e6e484e172
anis <>
2023-03-11 14:07:34 +0100

    fix css dropdown position + ENUM on factory

--- factories/RecipesFactory.js
+++ factories/RecipesFactory.js
@@ -1,8 +1,9 @@
 import { Recipe } from "../models/Recipe.js";
 
+export const RECIPES_DATA_TYPE = {JSON: "json data"};
 export class RecipesFactory {
     constructor(data, type) {
-        if (type === 'json') {
+        if (type === RECIPES_DATA_TYPE.JSON) {
             return new Recipe(data)
         } else {
             throw 'Unknown type format'
